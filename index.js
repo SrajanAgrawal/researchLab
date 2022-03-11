fd5482905679055c17ee0ae4024054463d30c97e
Srajan Agrawal <91273821+SrajanAgrawal@users.noreply.github.com>
2022-03-11 13:17:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,353 @@
+// About US Selectors
+var department = document.getElementById('department');
+var historys = document.getElementById('historySection');
+var visionAndMission = document.getElementById('visionAndMission');
+var objectives = document.getElementById('objectives');
+// Structure Selectors
+var incharges = document.getElementById('incharges');
+var faculty = document.getElementById('faculty');
+var advisoryCommittee = document.getElementById('advisoryCommittee');
+// Labs Selectors
+var reserachAndAILab = document.getElementById('reserachAndAILab');
+var researchAndTestingLab = document.getElementById('researchAndTestingLab');
+var advisoryCommitteeMeeting = document.getElementById('advisoryCommitteeMeeting');
+// Activities Selectors
+var trainingAndWorkshop = document.getElementById('trainingAndWorkshop');
+var projects = document.getElementById('projects');
+var testing = document.getElementById('testing');
+var patents = document.getElementById('patents');
+var papers = document.getElementById('papers');
+// Contact Us Selectors
+var contactUS = document.getElementById('contactUS')
+
+
+
+// *****************************Setting The Display Property*************************
+department.style.visibility = 'visible';
+historys.style.visibility = 'hidden';
+visionAndMission.style.visibility = 'hidden';
+objectives.style.visibility = 'hidden';
+incharges.style.visibility = 'hidden';
+faculty.style.visibility = 'hidden';
+advisoryCommittee.style.visibility = 'hidden';
+reserachAndAILab.style.visibility = 'hidden';
+researchAndTestingLab.style.visibility = 'hidden';
+advisoryCommitteeMeeting.style.visibility = 'hidden';
+trainingAndWorkshop.style.visibility = 'hidden';
+projects.style.visibility = 'hidden';
+testing.style.visibility = 'hidden';
+patents.style.visibility = 'hidden';
+papers.style.visibility  = 'hidden';
+contactUS.style.visibility = 'hidden';
+
+
+// About Us Section And It's Function
+function departmentFuction() {
+  department.style.visibility = 'visible';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+}
+function historyFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'visible';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function visionAndMissionFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'visible';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function objectivesFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'visible';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+// Structure Section And it's functions
+function inchargesFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'visible';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function facultyFuction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'visible';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+
+function advisoryCommitteeFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'visible';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+
+ // Labs section and it's function
+function reserachAndAILabFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'visible';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function researchAndTestingLabFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'visible';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+// Activities Function
+function advisoryCommitteeMeetingFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'visible';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function trainingAndWorkshopFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'visible';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function projectsFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'visible';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function testingFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'visible';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function patentsFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'visible';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'hidden';
+
+}
+function papersFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'visible';
+  contactUS.style.visibility = 'hidden';
+
+}
+
+// Contact US function
+function contactUSFunction() {
+  department.style.visibility = 'hidden';
+  historys.style.visibility = 'hidden';
+  visionAndMission.style.visibility = 'hidden';
+  objectives.style.visibility = 'hidden';
+  incharges.style.visibility = 'hidden';
+  faculty.style.visibility = 'hidden';
+  advisoryCommittee.style.visibility = 'hidden';
+  reserachAndAILab.style.visibility = 'hidden';
+  researchAndTestingLab.style.visibility = 'hidden';
+  advisoryCommitteeMeeting.style.visibility = 'hidden';
+  trainingAndWorkshop.style.visibility = 'hidden';
+  projects.style.visibility = 'hidden';
+  testing.style.visibility = 'hidden';
+  patents.style.visibility = 'hidden';
+  papers.style.visibility  = 'hidden';
+  contactUS.style.visibility = 'visible';
+}
